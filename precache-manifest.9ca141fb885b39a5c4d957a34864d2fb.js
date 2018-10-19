d4c352cdbb115409e27e5d1c682b99a155fbc50f
Mohammed Ehab <mohammed.ehab@sap.com>
2018-10-19 18:00:41 +0530

    Updates

---
+++ precache-manifest.9ca141fb885b39a5c4d957a34864d2fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "b1b958b974c16153088b",
+    "url": "/contactmanager/static/js/main.b1b958b9.chunk.js"
+  },
+  {
+    "revision": "54e7ce62ddbd32b22a5c",
+    "url": "/contactmanager/static/js/1.54e7ce62.chunk.js"
+  },
+  {
+    "revision": "b1b958b974c16153088b",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "54e7ce62ddbd32b22a5c",
+    "url": "/contactmanager/static/css/1.ae345905.chunk.css"
+  },
+  {
+    "revision": "2ae28c2b87b1862e1497924769f304a5",
+    "url": "/contactmanager/index.html"
+  }
+];
